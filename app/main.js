ff2a0face2e67f952071f77e6052152265f361c4
Jeremy Towne <jeremy.towne@skillsoft.com>
2019-09-19 15:31:26 -0400

    reformat with prettier

--- app/main.js
+++ app/main.js
@@ -4,8 +4,11 @@ const NODE_ENV = process.env.NODE_ENV || 'development'
 const PORT = process.env.PORT || '3000'
 const HOST = process.env.HOST || 'localhost'
 
-app.listen(PORT, HOST, () => process.stdout.write(`Server is listening on ${PORT} (${NODE_ENV})\n`))
-    .on('error', (err) => {
-        process.stdout.write(`Error occurred ${err.message}\n`)
-        setTimeout(() => process.exit(1), 5000)
-    })
+app
+  .listen(PORT, HOST, () =>
+    process.stdout.write(`Server is listening on ${PORT} (${NODE_ENV})\n`)
+  )
+  .on('error', err => {
+    process.stdout.write(`Error occurred ${err.message}\n`)
+    setTimeout(() => process.exit(1), 5000)
+  })
